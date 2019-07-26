7fd17594add6ab7d8e830fa50b479e3c5e099545
gideon <gideon@sabinet.co.za>
2019-07-26 21:32:30 +0200

    Sorted out composer and autoloading.

--- index.php
+++ index.php
@@ -7,12 +7,13 @@ require_once __DIR__ . '/vendor/autoload.php'; // Autoload files using Composer 
 
 use CmdPDF\Wkhtmltopdf;
 
+// Test 1
 $pdf1 = new Wkhtmltopdf();
 $pdf1->setFileName('test.pdf');
 $pdf1->url2pdf('http://www.google.com');
 
-$test_html = file_get_contents('https://google.com');
-
+// Test 2
+$test_html = "<h1>Some test HTML</h1>";
 $pdf2 = new Wkhtmltopdf();
 $pdf2->setFileName('test2.pdf');
 $pdf2->htmlString2pdf($test_html);
