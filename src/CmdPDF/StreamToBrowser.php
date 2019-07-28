635ab2dac56fcdcd86d078ad463954083775aa31
gideon <gideon@sabinet.co.za>
2019-07-28 10:38:42 +0200

    Added some class scafholding for new features.

---
+++ src/CmdPDF/StreamToBrowser.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace CmdPDF;
+
+/**
+ * This is a very simple class which concerns itself with streaming the content of a file
+ * to the browser.
+ *
+ * Class StreamToBrowser
+ * @package CmdPDF
+ * @author Gideon Schoonbee <project@mortolio.com>
+ * @license MIT
+ */
+class StreamToBrowser
+{
+    public function __construct()
+    {
+
+    }
+}
